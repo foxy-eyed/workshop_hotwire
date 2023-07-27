f57a172c7c2a07bf5cc9e92815a0c2e3e99beaab
Anna Selivanova <sean@servers.com>
2023-07-27 16:32:40 +0300

    Implement infinite scroll with frames

--- app/controllers/artists/tracks_controller.rb
+++ app/controllers/artists/tracks_controller.rb
@@ -2,13 +2,30 @@ class Artists::TracksController < ApplicationController
   TRACKS_PER_PAGE = 25
 
   def index
-    tracks = artist_resource.tracks.ordered.limit(TRACKS_PER_PAGE)
-    render action: :index, locals: {artist: artist_resource, tracks:}
+    tracks = artist_resource.tracks.ordered.limit(pagination_params[:limit]).offset(pagination_params[:offset])
+
+    if turbo_frame_request?
+      render partial: "list", locals: {artist: artist_resource, tracks:, **pagination_params}
+    else
+      render action: :index, locals: {artist: artist_resource, tracks:, **pagination_params}
+    end
   end
 
   private
 
   def artist_resource
     @artist_resource ||= Artist.find(params[:artist_id])
+  end
+
+  def pagination_params
+    @pagination_params ||= begin
+      page = params.fetch(:page, 0).to_i
+      limit = params.fetch(:limit, TRACKS_PER_PAGE).to_i
+      offset = limit * page
+      total_pages = artist_resource.tracks.count / limit
+      next_page = (page < total_pages) ? page + 1 : nil
+
+      {current_page: page, limit:, offset:, next_page:, total_pages:}
+    end
   end
 end
